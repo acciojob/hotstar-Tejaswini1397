84acb41fa10fad9fe1c834ee7b8ced5c71b4af55
Tejaswini Khairnar <tejaswinikhairnar97@gmail.com>
2023-08-04 16:11:51 +0530

    add

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -33,21 +33,36 @@ public class UserService {
 
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
-        User user=userRepository.findById(userId).get();
+     //   User user=userRepository.findById(userId).get();
+//        Integer age=0;
+//        if(user.getAge()<=18){
+//            age=18;
+//        }else {
+//            age=Integer.MAX_VALUE;
+//        }
+//        List<WebSeries>webSeriesList=webSeriesRepository.findAll();
+//        Integer count=0;
+//        for (WebSeries webSeries:webSeriesList){
+//
+//            if(webSeries.getAgeLimit()<=18)count++;
+//        }
+//
+//        return count;
+        User user =userRepository.findById(userId).get();
         Integer age=0;
-        if(user.getAge()<=18){
-            age=18;
-        }else {
-            age=Integer.MAX_VALUE;
-        }
-        List<WebSeries>webSeriesList=webSeriesRepository.findAll();
-        Integer count=0;
-        for (WebSeries webSeries:webSeriesList){
+        if(user.getAge()<=18)age=18;
+        else age=Integer.MAX_VALUE;
+        List<WebSeries> webSeriesList= webSeriesRepository.findAll();
 
-            if(webSeries.getAgeLimit()<=18)count++;
+        Integer count=0;
+
+        for(WebSeries webSeries:webSeriesList){
+            if(webSeries.getAgeLimit()<=age)count++;
         }
+
 
         return count;
+    }
     }
 
 
